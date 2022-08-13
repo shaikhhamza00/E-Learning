7246e354fb2ace130c1d354d81678a254f6d8c76
shaikhhamza00 <105962237+shaikhhamza00@users.noreply.github.com>
2022-08-13 19:13:10 +0500

    Create Severz.java

---
+++ Severz.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.net.*;
+
+
+public class Serverz {
+	static int c=0;
+	public static void main(String a[]) throws IOException {
+		
+			ServerSocket s=new ServerSocket(4553);
+			System.out.println("Server is ready waiting for client");
+			
+			Socket sr=s.accept();
+			
+			System.out.println("Client request granted");
+			
+			FileInputStream fr=new FileInputStream("D:\\study\\DB\\Lab 4\\Lab 04 Database.docx");
+			byte b[]=new byte[3000];
+			fr.read(b,0,b.length);
+			OutputStream os=sr.getOutputStream();
+	
+			os.write(b,0,b.length);
+			
+			
+			
+		
+	}
+}
